114b2df5fa1169935cab1bb9327e37735edd7581
Jasper van Riet <jaspervanriet@gmail.com>
2023-01-13 21:47:50 +0100

    Fix test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:showcase/design/theme.dart';
-import 'package:showcase/modules/gallery/gallery.dart';
 import 'package:showcase/modules/gallery/gallery_page.dart';
 
 void main() {
