8e393be68a0fa33ed0c59dfb703ac4bb866bf4e6
Jasper van Riet <jaspervanriet@gmail.com>
2023-01-13 21:42:53 +0100

    Allow switching of theme

--- lib/modules/gallery/gallery_page.dart
+++ lib/modules/gallery/gallery_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:showcase/design/system.dart';
 import 'package:showcase/design/theme.dart';
 import 'package:showcase/modules/gallery/gallery.dart';
 
@@ -10,8 +11,20 @@ class GalleryPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: ref.watch(theme).backgroundColor,
-      body: const SafeArea(
-        child: GalleryCard(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Expanded(
+              child: GalleryCard(),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  ref.read(theme.notifier).switchTheme();
+                },
+                child: const Text('Switch theme')),
+            SizedBox(height: ref.watch(designSystem).padding.medium),
+          ],
+        ),
       ),
     );
   }
